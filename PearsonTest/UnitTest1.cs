fb5c308a5345e24145d765e4de559cf4feac0e83
庄文杰 <909044055@qq.com>
2018-12-05 16:14:19 +0800

    add test

--- PearsonTest/UnitTest1.cs
+++ PearsonTest/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pearson;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PearsonTest
 {
@@ -21,6 +23,35 @@ namespace PearsonTest
             Assert.AreEqual(new PearsonCorrelationCoefficient(new double[] { 0, 0, 0, 0 }, new double[] { 0, 0, 0, 0 }).Score(), 0);
 
             Assert.AreEqual(new PearsonCorrelationCoefficient(new double[] { 0, 1, 0, 0, 52, 48 }, new double[] { 2, 4, 6, 0, 85, 44 }).Score(), 0.94);
+
+
+            List<TestClass> list = new List<TestClass>() {
+                new TestClass(){ col1="5",col2="8",col3="88",col4="98"},
+                new TestClass(){ col1="45",col2="30",col3="88",col4="98"},
+                new TestClass(){ col1="50",col2="25",col3="88",col4="98"},
+                new TestClass(){ col1="70",col2="50",col3="88",col4="98"},
+                new TestClass(){ col1="80",col2="85",col3="88",col4="98"}
+            };
+
+            var pearson = new PearsonCorrelationCoefficient(list.Select(a => new PearsonEntity<string>() { Col1 = a.col1, Col2 = a.col2 }).ToList()).Score();
+            Assert.AreEqual(pearson == 0.89, true);
+
+
+
+
         }
+
+
     }
+
+
+    public class TestClass
+    {
+        public string col1 { get; set; }
+        public string col2 { get; set; }
+        public string col3 { get; set; }
+        public string col4 { get; set; }
+
+    }
+
 }
